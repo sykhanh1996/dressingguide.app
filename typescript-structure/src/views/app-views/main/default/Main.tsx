1e427e92c4aabd520f9d054c4fef13d6ba019e3f
Khanh Sy <sykhanh1996@gmail.com>
2023-01-04 00:43:15 +0700

    style

--- typescript-structure/src/views/app-views/main/default/Main.tsx
+++ typescript-structure/src/views/app-views/main/default/Main.tsx
@@ -1,22 +1,25 @@
-import { Col, Row } from 'antd';
+import { Col, Divider, Row } from 'antd';
 import clsx from 'clsx';
+import { Fragment } from 'react';
 import styles from './Main.module.scss';
 
 export const Main = () => {
     return (
-        <Row justify="center" className={clsx(styles.main)}>
-            <Col span={16} className={clsx(styles.col)}>
-                <h1>
-                    <span>SportGear</span>
-                    <span>.</span>
-                    <span>how?</span>
-                </h1>
-                <p className={clsx('mgt-30')}>Explain sport gears using</p>
-                <div className={clsx('mgt-30')}>
-                    <div className={clsx('search-title')}>title</div>
-                    <div>searchBar</div>
-                </div>
-            </Col>
-        </Row>
+        <Fragment>
+            <Row justify="center" className={clsx(styles.main)} style={{ backgroundColor: '#fff' }}>
+                <Col span={16}>
+                    <h1>
+                        <span>SportGear</span>
+                        <span>.</span>
+                        <span>how?</span>
+                    </h1>
+                    <p className={clsx('mgt-30')}>Explain sport gears using</p>
+                    <div className={(clsx('mgt-30'), styles.search)}>
+                        <div>Search</div>
+                        <div>searchBar</div>
+                    </div>
+                </Col>
+            </Row>
+        </Fragment>
     );
 };
