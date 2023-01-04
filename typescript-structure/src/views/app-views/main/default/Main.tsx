cdafcc6e3a9bd7467797efa7826de55d08604e35
Sy Khanh Dell Laptop <sykhanh1996@gmail.com>
2023-01-04 11:35:38 +0700

    basic style for main page

--- typescript-structure/src/views/app-views/main/default/Main.tsx
+++ typescript-structure/src/views/app-views/main/default/Main.tsx
@@ -1,22 +1,54 @@
-import { Col, Divider, Row } from 'antd';
+import { DribbbleOutlined, GithubOutlined, SkypeOutlined, TwitterOutlined } from '@ant-design/icons';
+import { Col, Divider, Row, Space } from 'antd';
+import Search from 'antd/es/input/Search';
 import clsx from 'clsx';
 import { Fragment } from 'react';
 import styles from './Main.module.scss';
 
 export const Main = () => {
+    const onSearch = (value: string) => console.log(value);
+
     return (
         <Fragment>
             <Row justify="center" className={clsx(styles.main)} style={{ backgroundColor: '#fff' }}>
-                <Col span={16}>
-                    <h1>
-                        <span>SportGear</span>
-                        <span>.</span>
-                        <span>how?</span>
-                    </h1>
-                    <p className={clsx('mgt-30')}>Explain sport gears using</p>
-                    <div className={(clsx('mgt-30'), styles.search)}>
-                        <div>Search</div>
-                        <div>searchBar</div>
+                <Col span={8}>
+                    <Space direction="vertical">
+                        <h1>
+                            <span>SportGear</span>
+                            <span>.</span>
+                            <span>how?</span>
+                            <GithubOutlined style={{ fontSize: 15 }} />
+                        </h1>
+                        <p>Explain sport gears using</p>
+                    </Space>
+
+                    <div className={clsx('mgt-30', styles.search)}>
+                        <Row justify="start">
+                            <Col span={4}>
+                                <Space direction="horizontal">
+                                    <GithubOutlined style={{ fontSize: 15 }} />
+                                    <DribbbleOutlined style={{ fontSize: 15 }} />
+                                    <SkypeOutlined style={{ fontSize: 15 }} />
+                                </Space>
+                            </Col>
+                            <Col span={4} offset={16}>
+                                <Space direction="horizontal">
+                                    <GithubOutlined style={{ fontSize: 15 }} />
+                                    <DribbbleOutlined style={{ fontSize: 15 }} />
+                                    <SkypeOutlined style={{ fontSize: 15 }} />
+                                </Space>
+                            </Col>
+                        </Row>
+                        {/* <div className={clsx(styles.searchTitle)}>
+                                <GithubOutlined style={{ fontSize: 15 }} />
+                            </div> */}
+
+                        <Divider orientation="center" plain style={{ marginTop: 0, color: '#141414' }}>
+                            Search
+                        </Divider>
+                        <Space direction="vertical">
+                            <div className={clsx(styles.searchBar)}>searchBar</div>
+                        </Space>
                     </div>
                 </Col>
             </Row>
