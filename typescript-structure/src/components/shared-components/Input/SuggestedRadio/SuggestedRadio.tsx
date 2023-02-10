566e1fe4a341ee04ca9e9603e8e234aa8da176cb
Sy Khanh Dell Laptop <sykhanh1996@gmail.com>
2023-02-10 11:46:28 +0700

    component suggested radio

--- typescript-structure/src/components/shared-components/Input/SuggestedRadio/SuggestedRadio.tsx
+++ typescript-structure/src/components/shared-components/Input/SuggestedRadio/SuggestedRadio.tsx
@@ -1,52 +1,23 @@
 import clsx from 'clsx';
 import styles from './SuggestedRadio.module.scss';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { ISuggestedRadioProp } from './SuggestedRadio.view-model';
 
 const SuggestedRadio = (props: ISuggestedRadioProp) => {
-    const inputClassName = 'peer/' + props.color;
-
-    const labelClassName =
-        'peer-checked/' +
-        props.color +
-        ':border-4 peer-checked/' +
-        props.color +
-        ':border-' +
-        props.color +
-        '-500 peer-checked/' +
-        props.color +
-        ':scale-110 bg-' +
-        props.color +
-        '-400' +
-        ' ';
+    const [checked, setChecked] = useState(false);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const labels = document.querySelectorAll(`.${styles.suggestedItem}>label`);
-
-        labels.forEach((item) => {
-            if ((item as HTMLLabelElement).htmlFor == event.target.id) {
-                item.classList.add('border-4', 'border-' + props.colorName + '-500', 'scale-110');
-            } else {
-                item.classList.remove('border-4', 'border-' + props.colorName + '-500', 'scale-110');
-            }
-        });
+        if (event.target.checked) {
+            setChecked(true);
+        }
     };
-    // useEffect(() => {
-    //     const labels = document.querySelectorAll(`.${styles.suggestedItem}>label`);
-
-    //     labels.forEach((item) => {
-    //         if (props.color == item.textContent?.toLowerCase()) {
-    //             // item.st;
-    //         }
-    //     });
-    // }, []);
     return (
         <div className={clsx(styles.suggestedItem, 'flex items-center justify-center')}>
             <input type="radio" id={props.id} name="suggested-color" onChange={handleChange} />
             <label
                 htmlFor={props.id}
                 className={'grid place-items-center hover:scale-110 shadow-md labelColor'}
-                style={{ backgroundColor: props.color }}
+                style={{ backgroundColor: props.color, borderColor: checked ? props.borderColor : '' }}
             >
                 {/* capitalize the first letter */}
                 {props.colorName.charAt(0).toUpperCase() + props.colorName.slice(1)}
--- typescript-structure/src/views/app-views/main/official/Official.tsx
+++ typescript-structure/src/views/app-views/main/official/Official.tsx
@@ -14,31 +14,37 @@ export const Official = () => {
             color: 'rgb(34 197 94)',
             id: 'sg-green',
             colorName: 'green',
+            borderColor: 'rgb(74 222 128)',
         },
         {
             color: 'rgb(59 130 246)',
             id: 'sg-blue',
             colorName: 'blue',
+            borderColor: 'rgb(96 165 250)',
         },
         {
             color: 'rgb(239 68 68)',
             id: 'sg-red',
             colorName: 'red',
+            borderColor: 'rgb(252 165 165)',
         },
         {
             color: 'rgb(234 179 8)',
             id: 'sg-yellow',
             colorName: 'yellow',
+            borderColor: 'rgb(250 204 21)',
         },
         {
             color: 'rgb(14 165 233)',
             id: 'sg-sky',
             colorName: 'sky',
+            borderColor: 'rgb(56 189 248)',
         },
         {
             color: 'rgb(107 114 128)',
             id: 'sg-gray',
             colorName: 'gray',
+            borderColor: 'rgb(156 163 175)',
         },
     ];
     return (
@@ -147,8 +153,9 @@ export const Official = () => {
                                         <input
                                             type="text"
                                             placeholder="Type here"
-                                            className="input input-bordered input-md w-full max-w-xs text-lg"
+                                            className="input input-bordered input-md w-full max-w-xs text-xl"
                                             value="#2317ff"
+                                            readOnly
                                         />
 
                                         <ul className={clsx(styles.colorItems, 'flex')}>
