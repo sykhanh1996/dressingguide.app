e35c814473909de98e3057dda9fe30203eb1ffd3
Khanh Sy <sykhanh1996@gmail.com>
2023-03-08 00:25:31 +0700

    structure module code

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,13 +1,7 @@
-import express, { Request, Response } from "express";
+import { IndexRoute } from "./module/index";
+import App from "./app";
 
-const port = process.env.PORT || 5000;
+const routes = [new IndexRoute()];
+const app = new App(routes);
 
-const app = express();
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("API is running");
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+app.listen();
