540f9da6bec64ae01b634b0ecac812b97c1765c9
Khanh Sy <sykhanh1996@gmail.com>
2023-03-11 23:14:01 +0700

    connect to mongodb

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -10,6 +10,7 @@ class App {
     this.port = process.env.PORT || 5000;
 
     this.initializeRoutes(routes);
+    this.connectToDatabase();
   }
 
   public listen() {
@@ -27,8 +28,8 @@ class App {
   private connectToDatabase() {
     try {
       const connectionString =
-        "mongodb+srv://sa:Admin@123@master.yujmxtz.mongodb.net/?retryWrites=true&w=majority";
-      mongoose.connect("mongodb://127.0.0.1/my_database");
+        "mongodb+srv://dressingguide:YYQJES2NDvUnJbZb@master.33sgh3o.mongodb.net/?retryWrites=true&w=majority";
+      mongoose.connect(connectionString);
       console.log("Database connected...");
     } catch (error) {
       console.log(`Connect to database error`);
